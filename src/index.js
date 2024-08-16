8163650ef187786a8d897238c263a62f315271ee
Aleksandr-Szestak <szestak@hotmail.com>
2024-08-16 16:49:05 +0300

    refactor: improve code of function

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@
 
 module.exports = function towelSort (matrix) {
 
-let i, j ,row, ord=true;
+let i, j ,row, ord = true;
 let rez = [];
 
-if (matrix !== undefined){
+if (matrix !== undefined) {
 
-  for(i=0;i<matrix.length;i++){
+  for(i = 0; i < matrix.length; i++) {
 
       row = matrix[i];
       
-      if (ord){
-        for(j=0;j<row.length;j++){
+      if (ord) {
+        for(j=0; j < row.length; j++) {
           rez.push(row[j]);
         }
       }
-      else{
-        for(j=row.length-1;j>=0;j--){
+      else {
+        for(j = row.length-1; j >= 0; j--) {
           rez.push(row[j]);
         }
       }
