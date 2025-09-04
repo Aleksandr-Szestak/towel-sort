1ba864b233c1bbc1bbea259670bd5b3c6f7b05e3
Aleksandr-Szestak <szestak@hotmail.com>
2025-09-04 10:19:41 +0300

    feat: add implementation of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  let i;
+  let j;
+  let row;
+  let ord = true;
+  const rez = [];
+  if (matrix !== undefined) {
+    for (i = 0; i < matrix.length; i += 1) {
+      row = matrix[i];
+      if (ord) {
+        for (j = 0; j < row.length; j += 1) {
+          rez.push(row[j]);
+        }
+      } else {
+        for (j = row.length - 1; j >= 0; j -= 1) {
+          rez.push(row[j]);
+        }
+      }
+      ord = !ord;
+    }
+  }
+  return rez;
 };
